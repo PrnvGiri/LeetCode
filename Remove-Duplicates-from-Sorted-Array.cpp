7720c9aa1b96087f9893878c7c291c349676a5ed
Pranav Giri <114610311+PrnvGiri@users.noreply.github.com>
2023-09-17 01:36:49 +0530

    Update Remove-Duplicates-from-Sorted-Array.cpp

--- Remove-Duplicates-from-Sorted-Array.cpp
+++ Remove-Duplicates-from-Sorted-Array.cpp
@@ -1,1 +1,16 @@
-
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); ) { 
+                if (nums.at(i) == nums.at(j)) {
+                    nums.erase(nums.begin() + j);
+                }
+                else{
+                    j++;
+                }
+            }
+        }
+        return nums.size();
+    }
+};
